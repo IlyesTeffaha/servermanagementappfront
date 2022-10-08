96326312e0e31686d996aad1f26e8ade6f51a7b3
IlyesTeffaha <ilyes.teffaha.work@gmail.com>
2022-10-08 23:33:48 +0100

    completed response interface

--- src/app/interface/custom-response.ts
+++ src/app/interface/custom-response.ts
@@ -1,7 +1,13 @@
+import { Server } from "./server";
+
 export interface CustomResponse {
 
     timeStamp:Date;
     statusCode:number;
     status:string;
     reason:string;
+    message:string;
+    developerMessage:string;
+    //the question mark let's us tell typescript that these things are optional
+    data:{servers?:Server[],server?:Server};
 }
